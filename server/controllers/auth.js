4dc1afc1adbc45f0bdb7e5c093f17bfb6f4e26f8
shayash22 <67068946+shayash22@users.noreply.github.com>
2024-12-05 19:46:13 +0530

    check if user provide valid email.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,13 +2,23 @@
 export const welcome = (req,res) => {
     //using res.json(); we can send some data to the response to the client.
     res.json({
-        data: "hello from nodejs api",
+        data: "hello from nodejs api. ",
     });
 };
 
 export const preRegister = async (req, res) => {
+    //create jwt with email and password then email as clickable link.
+    //only when user click on that email link, registeration completes.
     try {
-        //
+        console.log(req.body);
+
+        const emailSent = true;
+
+        if(emailSent){
+            return res.json({ok: true});
+        }else{
+            return res.json({ ok: false});
+        }
     } catch (err) {
         console.log(err)
         return res.json({error: 'Something went wrong try again.'});
